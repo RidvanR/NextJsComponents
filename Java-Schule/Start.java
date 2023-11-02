b3efa18a23094a54a9b577fbdb1a3c7b2afdf482
RidvanR <rridvan@gmx.de>
2023-11-02 13:59:07 +0100

    Neues Projekt

---
+++ Java-Schule/Start.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Start {
+public static void main(String[] args) {
+        if (condition) statement;
+
+        if (Bedingung) {
+            Befehl;
+            }
+
+            if (condition) {
+                statements;
+                } else {
+                statements;
+                }
+                
+            
+    }
+}
